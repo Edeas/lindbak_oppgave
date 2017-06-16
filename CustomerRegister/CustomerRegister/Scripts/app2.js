ab683895335413ada10bb1a6cc8feba6cb8e2603
Endre Larsen <endrl@stud.ntnu.no>
2017-06-16 08:51:31 +0200

    Fixed empty field entry

--- CustomerRegister/CustomerRegister/Scripts/app2.js
+++ CustomerRegister/CustomerRegister/Scripts/app2.js
@@ -30,14 +30,20 @@ function RegisterViewModel() {
 
     self.addCustomer = function () {
         var c = new Customer(self.newEmail(), self.newFirstName(), self.newLastName(), self.newAddress(), self.newPostalCode(), -1);
+        // Check if fields are empty
+        if (c.email() == undefined) return;
+        if (c.firstName() == undefined) return;
+        if (c.lastName() == undefined) return;
+        if (c.address() == undefined) return;
+        if (c.postalCode() == undefined) return;
         self.customers.push(c);
         $.post("/api/Register/AddCustomer", c, function (result) { });
 
-        self.newEmail("");
-        self.newFirstName("");
-        self.newLastName("");
-        self.newAddress("");
-        self.newPostalCode("");
+        self.newEmail(undefined);
+        self.newFirstName(undefined);
+        self.newLastName(undefined);
+        self.newAddress(undefined);
+        self.newPostalCode(undefined);
     };
 
     self.updateCustomer = function (customer) {
